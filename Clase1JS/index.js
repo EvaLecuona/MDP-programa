080689df42bc2b44f07640c6139ec8361eb69940
EvaLecuona <104174724+EvaLecuona@users.noreply.github.com>
2023-07-20 21:23:55 +0000

    Add files via upload

---
+++ Clase1JS/index.js
@@ -0,0 +1,15 @@
+var a=3;
+var b=2;
+
+console.log(a+b)
+
+var c=9;
+var d= 7;
+console.log(c>d)
+console.log(d>c)
+
+let nombre = 'Eva';
+let edad = 21;
+console.log(nombre + edad)
+
+console.log('Eva');
